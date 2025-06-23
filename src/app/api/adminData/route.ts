9d9e372e5de7f191d183a67306648f82ac0026cd
JordanAryaLeksana <jordanaryaleksana41@gmail.com>
2025-06-24 06:30:25 +0700

    fix/admin api route.ts

--- src/app/api/adminData/route.ts
+++ src/app/api/adminData/route.ts
@@ -1,12 +1,10 @@
-// import { NextResponse } from "next/server";
-// import { createDefaultAdmin } from "@/utils/createAdmin";
+import { NextResponse } from "next/server";
 
-// export async function GET() {
-//   try {
-//     await createDefaultAdmin();
-//     return NextResponse.json({ message: "Admin created if not exists" }, { status: 200 });
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json({ error: "Failed to create admin" }, { status: 500 });
-//   }
-// }
+export async function GET() {
+  try {
+    return NextResponse.json({ message: "Hello Admin!!" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Failed to create admin" }, { status: 500 });
+  }
+}
